0d2a5aebdbb8e80824292f48b54bdd9f73266b7d
rurugit <neog.anupal101@gmail.com>
2019-08-30 19:11:41 +0530

    Add PUSH AND GET METHOD

--- src/main/java/com/example/NewApp/TestController.java
+++ src/main/java/com/example/NewApp/TestController.java
@@ -1,20 +1,23 @@
 package com.example.NewApp;
 
 // import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.GetMapping;
 // import org.springframework.web.bind.annotation.PostMapping;
 // import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 public class TestController{
 
+	//Route for default 
 	@RequestMapping("/")
 	@GetMapping
 	public String helloWorld(){
-		return "<h1>Hello Users!! Page is currently being build. other route '/users'</h1>";
+		return "<h1>Hello Users!! Page is currently being build. other route '/users', '/addusers' use postman</h1>";
 	}
 
 	@RequestMapping("/users")
@@ -22,6 +25,25 @@ public class TestController{
 	public String getUser(){
 		return "<h1>Get user is called</h1>";
 	}
+
+	//GET route
+	@RequestMapping("/addusers")
+	public String getData(@RequestParam(value = "userName", defaultValue="Anupal", required=false) String userName,
+						@RequestParam(value = "userPassword", defaultValue="notaprogrammeryet", required=false) String userPassword
+						){
+		
+		User user = new User(userName, userPassword);
+
+		return "GET method is called. Username is <b>" + user.getUserName() + "</b>. Password is <b>" + user.getUserPassword()+"</b>";
+		
+	}
+
+	//POST route
+	@RequestMapping(method = RequestMethod.POST, value = "/addusers")
+	public String registerUser(@RequestBody User user){
+
+		return "POST method is called. Username is <b>" + user.getUserName() + "</b>. Password is <b>" + user.getUserPassword()+"</b>";
+	}	
 }
 
 
